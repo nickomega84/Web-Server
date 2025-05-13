9f66f0d26ab794ecd0e8c00aa9cdb68f31d257ad
David Felipe Bonilla Ladino <dbonilla@c4r8s4.42urduliz.com>
2025-05-13 12:23:55 +0200

    Importante

---
+++ include/server/EpollServer.hpp
@@ -0,0 +1,59 @@
+#ifndef EPOLLSERVER_HPP
+#define EPOLLSERVER_HPP
+
+#include "IServer.hpp"
+#include <vector>
+#include <map>
+#include <string>
+#include "../router/Router.hpp"
+#include "../../include/core/Response.hpp"
+#include "../../include/middleware/MiddlewareStack.hpp"
+#include "../middleware/AllowMethodMiddleware.hpp"
+#include "../middleware/IMiddleware.hpp"
+
+
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cerrno>
+#include <cstring>
+#include <iostream>
+
+class EpollServer : public IServer {
+public:
+	// Canonical form (non-orthodox)
+	EpollServer();
+    // Router _router;
+
+	EpollServer(const EpollServer& other);
+	EpollServer& operator=(const EpollServer& other);
+	virtual ~EpollServer();
+
+    void setRouter(const Router& router); // ES IMPORTANTE
+    void setMiddlewareStack(const MiddlewareStack& stack); // ES IMPORTANTE
+
+	// Interface
+	void addListeningSocket(int port);
+	void start();
+	void stop();
+
+private:
+	int _epoll_fd;
+	std::vector<int>    _listenSockets;
+    Router _router;
+	MiddlewareStack _middleware;
+
+	std::map<int, std::string> _clients;
+
+	// Helpers
+	void setupEpoll();
+	void makeSocketNonBlocking(int fd);
+	void acceptConnection(int listen_fd);
+	void handleClientRead(int client_fd);
+	void handleClientWrite(int client_fd);
+	void closeClient(int client_fd);
+};
+
+#endif
