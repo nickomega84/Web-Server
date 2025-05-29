0d175a19dd091a6f8a6426e35c043b452342c8c5
nickomega84 <77319957+nickomega84@users.noreply.github.com>
2025-05-29 14:01:36 +0200

    Add files via upload

--- include/config/ConfigParser.hpp
+++ include/config/ConfigParser.hpp
@@ -6,20 +6,23 @@
 /*   By: nkrasimi <nkrasimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 17:57:19 by nkrasimi          #+#    #+#             */
-/*   Updated: 2025/05/28 18:02:05 by nkrasimi         ###   ########.fr       */
+/*   Updated: 2025/05/29 13:34:51 by nkrasimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <vector>
 
 class ConfigParser {
     private:
-        int			numServs;						// Cantidad total de servidores (uso interno)
+    	int	port;									// Puerto donde escucha
         std::string server_name;					// Nombre del servidor (directiva server_name)
         std::string host;							// IP o hostname donde escucha, puede estar vacío para que escuche a cualquier dirección IP
-        std::string	port;							// Puerto donde escucha
         std::string root;							// Ruta raíz general del servidor
-        std::string init_root;						// Copia de la raíz original (antes de ser modificada)
         std::string activeDirectory;				// Directorio activo en uso (puede cambiar en ejecución)
         std::string user;							// Usuario del proceso o sesión del servidor
         std::string index;							// Archivo por defecto a servir si no se especifica uno
@@ -29,6 +32,16 @@ class ConfigParser {
         bool		delete_allowed;					// DELETE permitido a nivel global
         bool		autoindex;			
     public:
+		bool get_GetAllowed() const;
+		bool get_PostAllowed() const;
+		bool get_DeleteAllowed() const;
+        bool get_Autoindex() const;
+
+		bool set_GetAllowed(bool &get_allowed);
+		bool set_PostAllowed(bool &post_allowed);
+		bool set_DeleteAllowed(bool &delete_allowed);
+        bool set_Autoindex(bool &autoindex);
+
         void parsingMethod() {
             std::cout << "Parsing is done" << std::endl;
         }
