d81212d44045bbe69eb5d406181ef3d74055e48b
Daniel Gonzalez Torralba <dangonz3@c4r8s1.42urduliz.com>
2025-06-04 09:46:44 +0200

    Signal class deleted

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ void signal_handler(int signum)
 {
 	if (signum == SIGINT || signum == SIGTERM)
 		g_signal_received = 1;
-	std::cout << "SEÑAL RECIBIDA" << std::endl;
+	std::cout << "Signal received. Closing" << std::endl;
 }
 
 int main() {
