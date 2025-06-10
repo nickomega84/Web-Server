f147a15af73e7b6488de1d6f3519fd90d3bb64c2
nickomega84 <77319957+nickomega84@users.noreply.github.com>
2025-06-10 15:35:10 +0200

    Add files via upload

--- src/utils/Utils.cpp
+++ src/utils/Utils.cpp
@@ -1,10 +1,22 @@
 #include "../../include/utils/Utils.hpp"
 #include <sstream>
 #include <map>
+#include <cctype>
 
 std::string Utils::intToString(int value) {
 	std::ostringstream oss;
 	oss << value;
 	return oss.str();
+}
+
+std::string trim(const std::string& str) {
+    if (str.empty()) return str;
+
+    size_t start = str.find_first_not_of(" \t\r\n");
+    size_t end = str.find_last_not_of(" \t\r\n");
+
+    if (start == std::string::npos || end == std::string::npos)
+        return "";
+    return str.substr(start, end - start + 1);
 }
 
