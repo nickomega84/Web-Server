b4ba7ad5d46ad19f07c20abe3898687200bed684
Daniel Gonzalez Torralba <dangonz3@c3r7s4.42urduliz.com>
2025-06-30 17:52:39 +0200

    Añadida metodo uploadResponse() a UploadHandler, eliminados metodos antiguos de UploadHandler

--- src/handler/UploadHandler.cpp
+++ src/handler/UploadHandler.cpp
@@ -13,52 +13,6 @@ UploadHandler::UploadHandler(const std::string& uploadsPath, IResponseBuilder* b
 
 UploadHandler::~UploadHandler() {}
 
-/* Response UploadHandler::handleRequest(const Request& req) 
-{
-    std::cout << "[DEBUG][UploadHandler][handleRequest] START" << std::endl;
-	
-	std::string method = req.getMethod();
-
-    if (method != "POST") {
-		std::cout << "[DEBUG][UploadHandler] not a POST method" << std::endl;
-        Payload payload;
-        payload.status = 405;
-        payload.reason = "Method Not Allowed";
-        payload.mime = "text/plain";
-        payload.body = "405 - Method Not Allowed";
-        payload.keepAlive = true;
-        return _builder->build(payload);
-    }
-
-    std::string body = req.getBody();
-    std::string uploadsPath = "./uploads"; // o cámbialo si es dinámico
-    std::string filename = "uploaded.txt"; // también puede ser dinámico
-    std::string fullpath = uploadsPath + "/" + filename;
-
-    std::ofstream file(fullpath.c_str());
-    if (!file.is_open()) {
-		std::cout << "[DEBUG][UploadHandler] 500 Cannot open the file" << std::endl;
-        Payload payload;
-        payload.status = 500;
-        payload.reason = "Internal Server Error";
-        payload.mime = "text/plain";
-        payload.body = "500 - No se pudo guardar el archivo";
-        payload.keepAlive = true;
-        return _builder->build(payload);
-    }
-
-    file << body;
-    file.close();
-
-    Payload payload;
-    payload.status = 200;
-    payload.reason = "OK";
-    payload.mime = "text/plain";
-    payload.body = "Archivo recibido y guardado.";
-    payload.keepAlive = true;
-    return _builder->build(payload);
-} */
-
 Response UploadHandler::handleRequest(const Request& request) 
 {
 	std::cout << "[DEBUG][UploadHandler][handleRequest] START" << std::endl;
@@ -68,89 +22,42 @@ Response UploadHandler::handleRequest(const Request& request)
 
 	std::string method = request.getMethod();
     if (method != "POST") 
-	{
-		std::cout << "[ERROR][UploadHandler] 405 not a POST method" << std::endl;
-        Payload payload;
-        payload.status = 405;
-        payload.reason = "Method Not Allowed";
-        payload.mime = "text/plain";
-        payload.body = "405 - Method Not Allowed";
-        payload.keepAlive = true;
-        return (_builder->build(payload));
-    }
+		return (std::cout << "[ERROR][UploadHandler] 405 not a POST method" << std::endl, \
+		uploadResponse(405, "Method Not Allowed", "text/plain", "405 - Method Not Allowed"));
 
 	std::string contentType = request.getHeader("content-type");
 	if (contentType.empty() || (contentType.find("multipart/form-data") == std::string::npos && \
 	contentType.find("text/plain") == std::string::npos))
-	{
-		std::cout << "[ERROR][UploadHandler] 400 unsupported format, contentType = " << contentType << std::endl;
-		Payload payload;
-		payload.status = 400;
-        payload.reason = "Bad Request";
-        payload.mime = "text/html";
-		return (_builder->build(payload));
-	}
+		return (std::cout << "[ERROR][UploadHandler] 400 unsupported format, contentType = " << contentType << std::endl, \
+		uploadResponse(400, "Bad Request", "text/html", ""));
 
 	std::string body = request.getBody();
 	std::string boundary = getBoundary(contentType);
 	if (boundary.empty())
-	{
-		std::cout << "[ERROR][UploadHandler] 400 Bad Request" << std::endl;
-		Payload payload;
-		payload.status = 400;
-		payload.reason = "Bad Request";
-		payload.mime = "text/html";
-		return (_builder->build(payload));
-	}
+		return (std::cout << std::cout << "[ERROR][UploadHandler] Cannot get boundary" << std::endl, \
+		uploadResponse(400, "Bad Request", "text/html", ""));
 
 	if (!parseMultipartBody(body, boundary, filename, fileContent))
-	{
-		std::cout << "[ERROR][UploadHandler] 400 Bad Request" << std::endl;
-		Payload payload;
-		payload.status = 400;
-		payload.reason = "Bad Request";
-		payload.mime = "text/html";
-		return (_builder->build(payload));
-	}
+		return (std::cout << std::cout << "[ERROR][UploadHandler] Cannot parse body" << std::endl, \
+		uploadResponse(400, "Bad Request", "text/html", ""));
 
 	if (filename.empty() || fileContent.empty())
-	{
-		std::cout << "[ERROR][UploadHandler] 400 Bad Request" << std::endl;
-		Payload payload;
-		payload.status = 400;
-        payload.reason = "Bad Request";
-        payload.mime = "text/html";
-		return (_builder->build(payload));
-	}
-
-	std::cout << "[DEBUG][UploadHandler] _uploadsPath = " << _uploadsPath << " filename = " << filename << std::endl;
+		return (std::cout << std::cout << "[ERROR][UploadHandler] Empty filename or content" << std::endl, \
+		uploadResponse(400, "Bad Request", "text/html", ""));
+	
 	std::string destinationPath = _uploadsPath + "/" + filename;
 	std::cout << "[DEBUG][UploadHandler] destinationPath = " << destinationPath << std::endl;
 
 	std::ofstream outputFile(destinationPath.c_str(), std::ios::out | std::ios::binary); // Es CRUCIAL abrir el archivo en modo binario (std::ios::binary)
 	if (!outputFile.is_open()) 
-	{
-		std::cout << "[ERROR][UploadHandler] 500 Cannot open file: " << destinationPath << std::endl;
-        Payload payload;
-		payload.status = 500;
-        payload.reason = "Internal Server Error";
-        payload.mime = "text/plain";
-        payload.body = "500 - No se pudo guardar el archivo";
-        payload.keepAlive = true;
-		return (_builder->build(payload));
-	}
+		return (std::cout << std::cout << "[ERROR][UploadHandler] 500 Cannot open file: " << std::endl, \
+		uploadResponse(500, "Internal Server Error", "text/plain", "500 - Cannot save file"));
 
 	outputFile.write(fileContent.data(), fileContent.size());
 	outputFile.close();
 
-	std::cout << "[DEBUG][UploadHandler] UPLOADED file: " << destinationPath << std::endl;
-	Payload payload;
-	payload.status = 200;
-    payload.reason = "OK";
-    payload.mime = "text/plain";
-    payload.body = "Archivo recibido y guardado";
-    payload.keepAlive = true;
-	return (_builder->build(payload));
+	return (std::cout << "[DEBUG][UploadHandler] UPLOADED file: " << destinationPath << std::endl, \
+	uploadResponse(200, "OK", "text/plain", "File received and uploaded"));
 }
 
 std::string UploadHandler::getBoundary(std::string& contentType)
@@ -185,9 +92,8 @@ bool UploadHandler::parseMultipartBody(const std::string& body, const std::strin
 
     // 4. Encontrar el separador entre cabeceras y contenido
     size_t headerEndPos = part.find("\r\n\r\n");
-    if (headerEndPos == std::string::npos) {
+    if (headerEndPos == std::string::npos)
         return false;
-    }
 
     std::string headers = part.substr(0, headerEndPos);
     // El contenido empieza 4 caracteres después del final de las cabeceras
@@ -218,106 +124,13 @@ bool UploadHandler::parseMultipartBody(const std::string& body, const std::strin
     return (!out_filename.empty());
 }
 
-
-// --- Cómo se integraría en el UploadHandler ---
-/*
-virtual Response handleRequest(const Request& request) {
-    Response res;
-
-    // A. Extraer el boundary de la cabecera Content-Type
-    std::string contentType = request.getHeader("Content-Type");
-    std::string boundary;
-    size_t boundaryPos = contentType.find("boundary=");
-    if (boundaryPos != std::string::npos) {
-        boundary = contentType.substr(boundaryPos + 9);
-    } else {
-        // Devolver error 400 Bad Request
-        return buildErrorResponse(400, "Bad Request: No boundary in Content-Type");
-    }
-
-    // B. Llamar a la función de parseo
-    std::string filename;
-    std::string fileContent;
-    if (!parseMultipartBody(request.getBody(), boundary, filename, fileContent)) {
-        // Devolver error 400 Bad Request
-        return buildErrorResponse(400, "Bad Request: Malformed multipart/form-data");
-    }
-
-    // C. Proceder con la lógica de guardado de archivo...
-    // ... (el resto del código del manejador)
+Response UploadHandler::uploadResponse(int status, std::string reason, std::string mime, std::string body) 
+{
+	Payload payload;
+	payload.status = status;
+	payload.reason = reason;
+	payload.mime = mime;
+	payload.body = body;
+	payload.keepAlive = true;
+	return (_builder->build(payload));
 }
-*/
-
-// Response UploadHandler::handleRequest(const Request& req) {
-//     if (req.getMethod() != "POST") {
-//         return _builder->buildErrorResponse(405, "Method Not Allowed");
-//     }
-
-//     std::string filename = "uploaded.txt"; // puedes mejorar con nombre dinámico
-//     std::string fullpath = _uploadsPath + "/" + filename;
-
-//     mkdir(_uploadsPath.c_str(), 0755); // crea directorio si no existe
-
-//     std::ofstream file(fullpath.c_str());
-//     if (!file.is_open()) {
-//         return _builder->buildErrorResponse(500, "No se pudo guardar el archivo");
-//     }
-
-//     file << req.getBody();
-//     file.close();
-
-//     Response res = _builder->buildEmptyResponse(200, "OK");
-//     res.setHeader("Content-Type", "text/plain");
-//     res.setBody("Archivo recibido y guardado.");
-//     res.setHeader("Content-Length", Utils::intToString(res.getBody().length()));
-//     return res;
-// }
-
-
-
-
-// Response UploadHandler::handleRequest(const Request& req) 
-// {
-// 	Response res;
-
-// 	std::string body = req.getBody();
-// 	std::ofstream file("uploads/files/uploaded.txt"); // ejemplo fijo
-// 	file << body;
-// 	file.close();
-
-// 	res.setStatus(200, "OK");
-// 	res.setHeader("Content-Type", "text/plain");
-// 	res.setBody("Archivo recibido y guardado.");
-// 	res.setHeader("Content-Length", Utils::intToString(res.getBody().length()));
-// 	return res;
-// }
-
-
-// Response UploadHandler::handleRequest(const Request& req) 
-// {
-//     Response res;
-
-//     std::string body = req.getBody();
-//     std::string uri = req.getURI(); // /uploads/archivo.txt
-//     std::string filename = uri.substr(uri.find_last_of("/") + 1);
-//     std::string fullpath = _uploadsPath + "/" + filename;
-
-//     // Crear directorios si no existen
-//     Utils::createDirectoriesIfNotExist(_uploadsPath);
-
-//     std::ofstream file(fullpath.c_str());
-//     if (!file) {
-//         res.setStatus(500, "Internal Server Error");
-//         res.setBody("No se pudo guardar el archivo.");
-//         return res;
-//     }
-
-//     file << body;
-//     file.close();
-
-//     res.setStatus(200, "OK");
-//     res.setHeader("Content-Type", "text/plain");
-//     res.setBody("Archivo '" + filename + "' recibido y guardado.");
-//     res.setHeader("Content-Length", Utils::intToString(res.getBody().length()));
-//     return res;
-// }
